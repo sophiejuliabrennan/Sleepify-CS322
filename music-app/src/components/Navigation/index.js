a248b619216a04aa5952a26dfeba5442ea0a7582
David john mc namara <david.mcnamara.2019@mumail.ie>
2020-10-28 13:23:16 +0000

    Added Page Navigation. Note is non functional

--- music-app/src/components/Navigation/index.js
+++ music-app/src/components/Navigation/index.js
@@ -1,9 +1,72 @@
 import React from 'react';
- 
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
 const Navigation = () => (
-  <div>
-    <h1>Navigation</h1>
-  </div>
+    <div className="NavBar" id="nav-container">
+        <nav>
+            <ul id="nav-list">    
+                <li>
+                    <Link to = { ROUTES.SIGN_IN }>
+                        Sign In
+                    </Link>
+                </li>
+                <li>
+                    <Link to = { ROUTES.SIGN_UP }>
+                        Create Account
+                    </Link>
+                </li>
+                <li>
+                    <Link to = { ROUTES.SIGN_OUT }>
+                        Sign Out
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link to = { ROUTES.HOME }>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                
+                    <input className={"searchBar"}type="text" placeholder="Search Music" name="search"/>
+                
+                </li>
+                <li>
+                    <Link to = { ROUTES.UPLOAD }>
+                        Upload
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link to = { ROUTES.ADMIN }>
+                        Admin 
+                    </Link>
+                </li>
+                <li>
+                    <li>
+                        <Link to = { ROUTES.ACCOUNT }>Account</Link>
+                    </li>
+                    {/* Drop down options */}
+                    <ul>
+                        <li>
+                            <Link to = { ROUTES.PROFILE }>Profile</Link>
+                        </li>
+                        <li>
+                            <Link to = { ROUTES.LIKES }>Likes</Link>
+                        </li>
+                        <li>
+                            <Link to = { ROUTES.PASSWORD_FORGET }>Forgot Password</Link>
+                        </li>
+                        <li>
+                            <Link to = { ROUTES.PASSWORD_CHANGE }>Change Password</Link>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
 );
  
 export default Navigation;
---
+++ music-app/src/constants/routes.js
@@ -0,0 +1,13 @@
+// Routes to the different pages of the application are specified in this file
+export const LANDING = "/";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
+export const SIGN_OUT = "/signout";
+export const HOME = "/home";
+export const ACCOUNT = "/account";
+export const UPLOAD = "/upload";
+export const ADMIN = "/admin";
+export const PASSWORD_FORGET = "/pass-f";
+export const PASSWORD_CHANGE = "/pass-c";
+export const PROFILE = "/profile";
+export const LIKES = "/likes";
