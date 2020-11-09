48810f67c548641b38b0bcb247e75f14d00c86ba
ArtsemHere <artsem.saldatsenka.2018@mumail.ie>
2020-11-09 22:07:30 +0000

    LandingPage

--- music-app/src/components/Landing/index.js
+++ music-app/src/components/Landing/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
  
 const Landing = () => (
-  <div>
+  <div1>
     <h1>Landing</h1>
-  </div>
+
+    <div2>
+    <h3>Sign In before listening to music:</h3>
+    </div2>
+
+   <Link className="one" to = { ROUTES.SIGN_IN }>Sign In</Link>
+  
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <div2>
+    <h3>If you have not created account yet, please Sign Up first:</h3>
+    </div2>
+
+    <Link className="one" to = { ROUTES.SIGN_UP }>Sign Up</Link>
+
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <h1>We are a free music streming website</h1>
+    <h2>And the features that this website provide include:</h2>
+    <h3>Any user can upload their music so you can listen to it online.</h3>
+    <h3>You may download songs you like or add them to your own play list.</h3>
+    <h3>Before listening to the songs, you have to create an accont with us.</h3>
+    <h3>You can use your Google account when signing up for faster access.</h3>
+    <h3>You can browse the music by songs, artists or genre.</h3>
+    <h3>You can do all of that in seconds and its free of charge.</h3>
+
+  </div1>
+
 );
+
+
+
+
  
 export default Landing;
