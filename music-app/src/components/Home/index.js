4f3cec3026ff13cc02935bd02a633f7e5b45b4d1
David john mc namara <david.mcnamara.2019@mumail.ie>
2020-11-10 16:47:19 +0000

    Sample Commit

--- music-app/src/components/Home/index.js
+++ music-app/src/components/Home/index.js
@@ -5,14 +5,12 @@ import { withAuthorization } from '../Session';
 
 function check(){
     var user = firebase.auth().currentUser;
-
     if (user) {
         console.log("Welcome " + user.email);
         document.getElementById("welcome").innerHTML = "Welcome " + user.email;
     } else {
         console.log("not signed in");
         document.getElementById("welcome").innerHTML = "You are not signed in.";
-        //document.getElementById("welcome").appendChild("SignUpLink");
     }
 }
 
@@ -21,7 +19,10 @@ const Home = () => (
   <div onLoad={check}>
     <h1 id="welcome" onLoad={check}></h1>
     <button onClick={check}>am i logged in?</button>
-    
+    <br/>
+    <div>
+      <h1> Music Goes Here</h1>
+    </div>
   </div>
 );
 
