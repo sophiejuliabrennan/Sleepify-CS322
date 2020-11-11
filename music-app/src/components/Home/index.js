1316cf49be392eb220aafcb336c0d4b450d50a15
David john mc namara <david.mcnamara.2019@mumail.ie>
2020-11-11 11:31:07 +0000

    Moved the card style to seperate css file

--- music-app/src/components/Home/index.js
+++ music-app/src/components/Home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import firebase from "firebase";
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
+import "./card.css";
 
 const Discover = () => (
   <div>
@@ -60,65 +61,33 @@ class GetMusicBase extends Component{
 
 function CreateCard(link, artist, genre, song){
   var card = document.createElement("div");
-
-  // abstract this style into a CSS file
-  card.style.border = "5px red";
-  card.style.backgroundColor = "grey";
-  card.style.textAlign = "left";
-  card.style.display = "inline-block";
-  card.style.padding = "1%";
-  card.style.margin = "1%";
+  card.id = "card";
 
   card.onclick = function(){console.log("im about to play " + link)};
 
   var _artist = document.createElement("div");
   var artist_text = document.createElement("H2");
+  artist_text.id = "artistID";
   artist_text.innerHTML = artist;
   _artist.appendChild(artist_text);
   card.appendChild(_artist);
 
   var _song = document.createElement("div");
   var song_text = document.createElement("H1");
+  song_text.id = "songID"; 
   song_text.innerHTML = song;
-  _song.style.textAlign = "center";
   _song.appendChild(song_text);
   card.appendChild(_song);
 
   var _genre = document.createElement("div");
   var genre_text = document.createElement("H2");
+  genre_text.id = "genreID"; 
   genre_text.innerHTML = genre;
   _genre.appendChild(genre_text);
-  _genre.style.textAlign = "right";
   card.appendChild(_genre);
 
   document.getElementById("music").appendChild(card);
 }
-
-function check(){
-    var user = firebase.auth().currentUser;
-    if (user) {
-        console.log("Welcome " + user.email);
-        document.getElementById("welcome").innerHTML = "Welcome " + user.email;
-    } else {
-        console.log("not signed in");
-        document.getElementById("welcome").innerHTML = "You are not signed in.";
-    }
-}
-
-
-// const Home = () => (
-//   <div onLoad={check}>
-//     <h1 id="welcome" onLoad={check}></h1>
-//     <button onClick={check}>am i logged in?</button>
-//     <br/>
-//     <div>
-//       <h1> Music Goes Here</h1>
-//       {
-        
-//       }
-//     </div>
-//   </div>
-// );
 
 const GetMusic = withFirebase(GetMusicBase);
 
