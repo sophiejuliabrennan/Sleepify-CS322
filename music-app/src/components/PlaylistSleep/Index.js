a875e8bad89e2353343a9f357a0a93019e04c5a4
https://gitlab.cs.nuim.ie/u180610 <sophie.brennan.2018@mumail.ie>
2021-12-09 13:22:08 +0000

    git add .

---
+++ music-app/src/components/PlaylistSleep/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+const PlaylistSleep = () => (
+    <div1>
+      <div>
+      <h1>Sleepify</h1>
+      <h2>Sleep Playlist</h2>
+      </div> 
+    </div1>
+  
+  );
+  
+  export default PlaylistSleep;
